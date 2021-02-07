74905848c40f27ef0a5bf4f9801426bdce419168
MasonChance <mason.fryberger@gmail.com>
2021-02-06 18:31:50 -0800

    encrypt/decrypt alphastring functions passing tests

--- caesar_cipher/cipher.py
+++ caesar_cipher/cipher.py
@@ -1,13 +1,42 @@
+import string
+
+
+
+# base form for encrypt-> decrypt. numeric only. 
+def encrypt_num_str(plain:str, key:int)-> str:
+  cryptid = ''
+  for char in plain:
+    cryptid += str((int(char) + key) %10)
+  return cryptid
+
+def decrypt_num_str(cryptid, key):
+  return encrypt(cryptid, -key)
 
 
 def encrypt(plain:str, key:int)-> str:
   cryptid = ''
+  table = {}
+  cypher = {}
+  for i, char in enumerate(string.ascii_lowercase):
+    table[f'{char}'] = i   
+    cypher[f'{(int(table[char]) - key) % 26}'] = char
+
   for char in plain:
-    cryptid += str(((int(char) + key) %10))
+    if char == ' ':
+      cryptid += ' '
+    else:
+      char = int(table[f'{char.lower()}'])
+      cryptid += cypher[f'{char}']
   return cryptid
+  
 
 def decrypt(cryptid, key):
   return encrypt(cryptid, -key)
+
+#first attempt brute force w/comparison to natural langlib 
+def crack(cryptid):
+  
+
 
 
 
@@ -15,9 +44,11 @@ if __name__ == "__main__":
   # 867  plain
   # 3    key
   # expected 190
-  print(encrypt("867", 3))
+  print(encrypt("abc", 3))
   # 190 cryptid in
   # 3 key (known)
   # expected 867
-  print(decrypt('190', 3))
-  
+  # print(decrypt('190', 3))
+  # 'five three oh nine' plain
+  # 3 key
+  # 'ilyh wkuhh rk qlqh'
--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -1,5 +1,15 @@
-from caesar_cipher import __version__
 
+import pytest
+from caesar_cipher.cipher import encrypt, decrypt, crack
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_alpha_encrypt():
+    jenny = 'five three oh nine'
+    assert encrypt(jenny, 3) == 'ilyh wkuhh rk qlqh' 
+    
+def test_alpha_decrypt():
+    gobbledy = 'ilyh wkuhh rk qlqh'
+    assert decrypt(gobbledy, 3) == 'five three oh nine'
+
+def test_alpha_crack():
+    gobbledy = 'ilyh wkuhh rk qlqh'
+    assert crack(gobbledy) == 'five three oh nine'
